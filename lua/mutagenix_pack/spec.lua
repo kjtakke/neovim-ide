035782979d4163a9932dabfea6ae1c3c2d6701c6
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2025-08-09 10:25:15 +1000

    Update spec.lua

--- lua/mutagenix_pack/spec.lua
+++ lua/mutagenix_pack/spec.lua
@@ -1,6 +1,4 @@
--- Returns a list of Lazy.nvim plug‑in specifications plus a core pseudo‑plug‑in.
-
--- Safe require helper used in config functions; avoids throwing on missing modules.
+-- lua/mutagenix_pack/spec.lua
 local function safe_require(mod)
   local ok, pkg = pcall(require, mod)
   return ok and pkg or nil
@@ -8,176 +6,143 @@ end
 
 local M = {}
 
--- Core pseudo‑plug‑in: runs our editor setup immediately (high priority)
-M[1] = {
-  name     = "mutagenix/core",
+-- 🔧 Core loader anchored to a real plugin so the spec is valid
+M[#M+1] = {
+  "nvim-lua/plenary.nvim",
+  lazy = false,
   priority = 10000,
-  lazy     = false,
-  config   = function()
+  init = function()
+    -- run your editor options, autocmds, keymaps, custom commands, etc.
     require("mutagenix_pack.core").setup()
   end,
 }
 
--- todo-comments.nvim
-table.insert(M, {
+-- todo-comments
+M[#M+1] = {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  event        = { "BufReadPost", "BufNewFile" },
-  opts         = {
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
     signs = true,
-    highlight = {
-      keyword = "bg",   -- highlight the whole keyword
-      after   = "fg",   -- colour the text after the keyword
-    },
+    highlight = { keyword = "bg", after = "fg" },
     search = {
       command = "rg",
-      args    = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column" },
+      args = { "--color=never","--no-heading","--with-filename","--line-number","--column" },
       pattern = [[\b(KEYWORDS):]],
     },
     keywords = {
       TODO  = { icon = " ", colour = "info" },
-      FIX   = { icon = " ", colour = "error",  alt = { "FIXME", "BUG" } },
+      FIX   = { icon = " ", colour = "error", alt = { "FIXME", "BUG" } },
       WARN  = { icon = " ", colour = "warning", alt = { "WARNING" } },
-      NOTE  = { icon = " ", colour = "hint",    alt = { "INFO" } },
+      NOTE  = { icon = " ", colour = "hint", alt = { "INFO" } },
       PERF  = { icon = " ", colour = "default", alt = { "PERFORMANCE" } },
       CHORE = { icon = " ", colour = "default" },
       IDEA  = { icon = "💡", colour = "hint" },
     },
   },
   keys = {
-    { "]t",  function() require("todo-comments").jump_next() end, desc = "Next TODO/FIX/WARN" },
-    { "[t",  function() require("todo-comments").jump_prev() end, desc = "Prev TODO/FIX/WARN"  },
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO/FIX/WARN" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Prev TODO/FIX/WARN"  },
     { "<leader>td", "<cmd>TodoTelescope<cr>", desc = "Todos (Telescope)" },
     { "<leader>tq", "<cmd>TodoQuickFix<cr>",  desc = "Todos → Quickfix" },
     { "<leader>tl", "<cmd>TodoLocList<cr>",   desc = "Todos → Loclist"  },
     { "<leader>tt", "<cmd>TodoTrouble<cr>",   desc = "Todos (Trouble)"  },
   },
-})
+}
 
 -- Markdown preview
-table.insert(M, {
+M[#M+1] = {
   "iamcco/markdown-preview.nvim",
-  ft    = { "markdown" },
+  ft = { "markdown" },
   build = "cd app && npm install",
-  init  = function()
-    vim.g.mkdp_auto_start         = 0
-    vim.g.mkdp_auto_close         = 1
-    vim.g.mkdp_refresh_slow       = 0
+  init = function()
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_refresh_slow = 0
     vim.g.mkdp_command_for_global = 0
-    vim.g.mkdp_open_to_the_world  = 0
-    vim.g.mkdp_open_ip            = "127.0.0.1"
-    vim.g.mkdp_port               = "8080"
-    vim.g.mkdp_theme              = "dark"
+    vim.g.mkdp_open_to_the_world = 0
+    vim.g.mkdp_open_ip = "127.0.0.1"
+    vim.g.mkdp_port = "8080"
+    vim.g.mkdp_theme = "dark"
   end,
   keys = {
     { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview (HTTP)" },
   },
-})
+}
 
--- Colourscheme: catppuccin with integrations
-table.insert(M, {
+-- Colourscheme
+M[#M+1] = {
   "catppuccin/nvim",
-  name     = "catppuccin",
+  name = "catppuccin",
   priority = 1000,
-  config   = function()
-    local cap = safe_require("catppuccin")
-    if cap then
-      cap.setup({
-        integrations = {
-          telescope = true,
-          nvimtree  = true,
-          cmp       = true,
-        },
-      })
+  config = function()
+    local c = safe_require("catppuccin")
+    if c then
+      c.setup({ integrations = { telescope = true, nvimtree = true, cmp = true } })
+      vim.cmd.colorscheme("catppuccin")
     end
-    vim.cmd.colorscheme("catppuccin")
   end,
-})
+}
 
--- GitHub Copilot client
-table.insert(M, {
-  "github/copilot.vim",
-  lazy = false,
-})
+-- Copilot
+M[#M+1] = { "github/copilot.vim", lazy = false }
 
--- Visual Multi (multi‑cursor)
-table.insert(M, {
-  "mg979/vim-visual-multi",
-  branch = "master",
-})
+-- Visual Multi
+M[#M+1] = { "mg979/vim-visual-multi", branch = "master" }
 
 -- LSP / Mason / Completion
-table.insert(M, {
-  "neovim/nvim-lspconfig",
-})
+M[#M+1] = { "neovim/nvim-lspconfig" }
 
-table.insert(M, {
+M[#M+1] = {
   "williamboman/mason.nvim",
   build = ":MasonUpdate",
   config = function()
     local mason = safe_require("mason")
     if mason then mason.setup() end
   end,
-})
+}
 
-table.insert(M, {
+M[#M+1] = {
   "williamboman/mason-lspconfig.nvim",
   dependencies = { "williamboman/mason.nvim" },
   config = function()
-    local mls = safe_require("mason-lspconfig")
-    if mls then
-      mls.setup({
+    local m = safe_require("mason-lspconfig")
+    if m then
+      m.setup({
         ensure_installed = {
-          -- Web
-          "html", "cssls", "emmet_ls", "vtsls", "eslint", "jsonls",
-          -- Backend / general purpose
-          "pyright", "bashls", "lua_ls", "gopls", "rust_analyzer", "clangd", "dockerls", "yamlls", "graphql",
-          -- JVM / XML / .NET
-          "jdtls", "kotlin_language_server", "lemminx", "omnisharp",
-          -- DB / Configs
-          "sqlls", "taplo",
-          -- Others
-          "perlpls", "powershell_es", "ansiblels", "terraformls",
-          -- Docs / Markdown
-          "marksman", "ltex",
+          "html","cssls","emmet_ls","vtsls","eslint","jsonls",
+          "pyright","bashls","lua_ls","gopls","rust_analyzer","clangd","dockerls","yamlls","graphql",
+          "jdtls","kotlin_language_server","lemminx","omnisharp",
+          "sqlls","taplo",
+          "perlpls","powershell_es","ansiblels","terraformls",
+          "marksman","ltex",
         },
       })
     end
   end,
-})
+}
 
-table.insert(M, {
+M[#M+1] = {
   "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-  },
+  dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
   config = function()
-    local cmp     = safe_require("cmp")
-    local luasnip = safe_require("luasnip")
-    if cmp and luasnip then
-      cmp.setup({
-        snippet = {
-          expand = function(args) luasnip.lsp_expand(args.body) end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"]     = cmp.mapping.select_next_item(),
-          ["<S-Tab>"]   = cmp.mapping.select_prev_item(),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip"  },
-        },
-      })
-    end
+    local cmp, luasnip = safe_require("cmp"), safe_require("luasnip")
+    if not (cmp and luasnip) then return end
+    cmp.setup({
+      snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"]     = cmp.mapping.select_next_item(),
+        ["<S-Tab>"]   = cmp.mapping.select_prev_item(),
+      }),
+      sources = { { name = "nvim_lsp" }, { name = "luasnip" } },
+    })
   end,
-})
+}
 
 -- Treesitter
-table.insert(M, {
+M[#M+1] = {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
@@ -185,81 +150,63 @@ table.insert(M, {
     if ts then
       ts.setup({
         ensure_installed = {
-          -- Web
-          "css", "html", "javascript", "json", "scss", "typescript",
-          -- Backend / General purpose
-          "bash", "c", "cpp", "elixir", "go", "java", "kotlin", "lua", "perl", "php", "python", "ruby", "rust",
-          -- Infra / Scripting
-          "dockerfile", "make", "terraform", "toml", "yaml",
-          -- Markup / Docs
-          "latex", "markdown", "markdown_inline",
-          -- Other
-          "graphql", "regex", "sql",
+          "css","html","javascript","json","scss","typescript",
+          "bash","c","cpp","elixir","go","java","kotlin","lua","perl","php","python","ruby","rust",
+          "dockerfile","make","terraform","toml","yaml",
+          "latex","markdown","markdown_inline",
+          "graphql","regex","sql",
         },
         highlight = { enable = true },
       })
     end
   end,
-})
+}
 
 -- File explorer
-table.insert(M, {
+M[#M+1] = {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle File Tree" },
-  },
+  keys = { { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle File Tree" } },
   config = function()
-    local nvtree = safe_require("nvim-tree")
-    if nvtree then
-      nvtree.setup({
-        view = {
-          width = 50,
-          side  = "left",
-          preserve_window_proportions = true,
-        },
-        filters = {
-          custom  = { ".git" },
-          exclude = {},
-        },
+    local t = safe_require("nvim-tree")
+    if t then
+      t.setup({
+        view = { width = 50, side = "left", preserve_window_proportions = true },
+        filters = { custom = { ".git" }, exclude = {} },
         respect_buf_cwd = false,
       })
     end
   end,
-})
+}
 
 -- Telescope
-table.insert(M, {
+M[#M+1] = {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local builtin = safe_require("telescope.builtin")
     if not builtin then return end
     local map = vim.keymap.set
-    map("n", "<leader>ff", builtin.find_files,  { desc = "Find Files"       })
-    map("n", "<leader>fg", builtin.live_grep,   { desc = "Live Grep"        })
-    map("n", "<leader>fb", builtin.buffers,     { desc = "Buffers"          })
-    map("n", "<leader>fh", builtin.help_tags,   { desc = "Help Tags"        })
-    map("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics"      })
-    map("n", "<leader>fr", builtin.resume,      { desc = "Resume"           })
+    map("n", "<leader>ff", builtin.find_files,  { desc = "Find Files" })
+    map("n", "<leader>fg", builtin.live_grep,   { desc = "Live Grep"  })
+    map("n", "<leader>fb", builtin.buffers,     { desc = "Buffers"    })
+    map("n", "<leader>fh", builtin.help_tags,   { desc = "Help Tags"  })
+    map("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics"})
+    map("n", "<leader>fr", builtin.resume,      { desc = "Resume"     })
   end,
-})
+}
 
 -- UI niceties
-table.insert(M, {
+M[#M+1] = {
   "nvim-lualine/lualine.nvim",
   config = function()
-    local line = safe_require("lualine")
-    if line then
-      line.setup({ options = { theme = "catppuccin" } })
-    end
+    local l = safe_require("lualine")
+    if l then l.setup({ options = { theme = "catppuccin" } }) end
   end,
-})
-table.insert(M, { "lewis6991/gitsigns.nvim", config = true })
-table.insert(M, { "windwp/nvim-autopairs",   config = true })
-table.insert(M, { "numToStr/Comment.nvim",   config = true })
-
--- Optional: vim-fugitive for :G blame support
-table.insert(M, { "tpope/vim-fugitive" })
+}
+M[#M+1] = { "lewis6991/gitsigns.nvim",  config = true }
+M[#M+1] = { "windwp/nvim-autopairs",    config = true }
+M[#M+1] = { "numToStr/Comment.nvim",    config = true }
+M[#M+1] = { "tpope/vim-fugitive" }
 
 return M
